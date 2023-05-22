b2c3700c6629ae326da1789cea7005e3f97a2438
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-05-22 19:25:20 +0900

    [add]CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,37 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    render json: @posts
+    posts = Post.all
+    render json: posts
+  end
+
+  def create
+    post = Object.new(post_params)
+    object.save
+    redirect_to object
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    render json: post
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    render json: post
+  end
+  
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 end
